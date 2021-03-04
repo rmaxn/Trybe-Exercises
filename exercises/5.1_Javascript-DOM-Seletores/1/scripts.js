fd050ea7a2d63da316cfc12f26a77466b9ce1901
Robertson <robertson.nunes@gmail.com>
2021-03-04 16:01:28 -0300

    exercício 2

--- exercises/5.1_Javascript-DOM-Seletores/1/scripts.js
+++ exercises/5.1_Javascript-DOM-Seletores/1/scripts.js
@@ -1,14 +1,21 @@
+// Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito.
 document.getElementById("page-title").innerText = "Harry Potter e o Enigma do Príncipe"
+
+// Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
+document.getElementById("second-paragraph").innerText = "Harry Potter and the Half-blood prince"
+
+// Por fim, recupere o subtítulo e altere-o também.
+document.getElementById("subtitle").innerText = "Título Original"
+
+//Recupere os seus parágrafos via código JavaScript , usando a função getElementsByClassName ;
+document.getElementsByClassName("firstParagraph")[0].innerText
+document.getElementsByClassName("secondParagraph")[0].innerText
+
+//Altere algum estilo do primeiro deles.
+document.getElementsByClassName("firstParagraph")[0].style.backgroundColor = "yellow"
+
+//Recupere o subtítulo pela tag.
+document.getElementsByTagName("p")[0].innerText
 
 var paragraph = document.getElementById("paragraph");
 paragraph.style.color = "red";
-
-document.getElementById("second-paragraph").innerText = "Harry Potter and the Half-blood prince"
-
-document.getElementById("subtitle").innerText = "Título Original"
-
-document.getElementsByClassName("firstParagraph")[0].innerText
-
-document.getElementsByClassName("secondParagraph")[0].innerText
-
-document.getElementsByTagName("p")[0].innerText
---
+++ exercises/5.1_Javascript-DOM-Seletores/2/scripts.js
@@ -0,0 +1,33 @@
+document.getElementsByTagName("div")[0].style.backgroundColor = "red";
+
+document.getElementsByTagName("div")[1].style.backgroundColor = "orange";
+
+document.getElementsByTagName("div")[2].style.backgroundColor = "yellow";
+
+document.getElementsByTagName("div")[3].style.backgroundColor = "green";
+
+document.getElementsByTagName("h1")[0].style.backgroundColor = "black";
+
+document.querySelectorAll("h3")[0].style.backgroundColor = "black";
+
+document.querySelectorAll("h3")[1].style.backgroundColor = "black";
+
+document.querySelectorAll("h3")[2].style.backgroundColor = "black";
+
+document.querySelectorAll("h3")[3].style.backgroundColor = "black";
+
+document.querySelectorAll("div")[0].style.borderStyle = "solid";
+
+document.querySelectorAll("div")[1].style.borderStyle = "solid";
+
+document.querySelectorAll("div")[2].style.borderStyle = "solid";
+
+document.querySelectorAll("div")[3].style.borderStyle = "solid";
+
+document.querySelectorAll("div")[0].style.borderColor = "black";
+
+document.querySelectorAll("div")[1].style.borderColor = "black";
+
+document.querySelectorAll("div")[2].style.borderColor = "black";
+
+document.querySelectorAll("div")[3].style.borderColor = "black";
