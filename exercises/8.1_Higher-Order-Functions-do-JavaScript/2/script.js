2e464b00480ad6381cd1d14d5b1eb5199112a382
Robertson <robertson.nunes@gmail.com>
2021-03-29 19:39:17 -0300

    8.1 higher order functions.2

--- exercises/8.1_Higher-Order-Functions-do-JavaScript/2/script.js
+++ exercises/8.1_Higher-Order-Functions-do-JavaScript/2/script.js
@@ -1,3 +1,4 @@
+/* 1. Crie uma função de primeira classe que retorna um objeto { nomeCompleto, email } de uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respctivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com. */
 const newEmployees = () => {
   const employees = {
     id1: generateEmployee('Pedro Guerra'),
@@ -12,3 +13,22 @@ generateEmployee = (name) => {
   return {name, email: `${email}@betrybe.com}`};
 }
 console.log(newEmployees(generateEmployee));
+
+/* Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou"). */
+
+
+const sorteio = () => {
+  const result = Math.round((Math.random() * 5) + 1);
+  return result;
+} 
+console.log(sorteio())
+
+const check = () => {
+  const aposta = Math.round((Math.random() * 5) + 1);
+  if (sorteio() === aposta) {
+  console.log('Parabéns você ganhou')
+} else {
+  console.log('Tente novamente')
+  } 
+} 
+check()
