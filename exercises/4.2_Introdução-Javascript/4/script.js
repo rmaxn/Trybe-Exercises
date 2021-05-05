83baee56aa836b7024606aa98602fe8fe4f58b77
Robertson <robertson.nunes@gmail.com>
2021-05-05 18:48:50 -0300

    Javascript-Array-4

--- exercises/4.2_Introdução-Javascript/4/script.js
+++ exercises/4.2_Introdução-Javascript/4/script.js
@@ -1,57 +1,65 @@
 //Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
-for (let index = 0; index < numbers.length; index++){
-    console.log(numbers[index])
+for (let index of numbers){
+    console.log(index)
 }
+
 //Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
 let resultado = 0;
-for (let contador = 0; contador < numbers.length; contador++){
-    resultado += numbers[contador]
+for (let contador of numbers){
+    resultado += contador
 }
     console.log(resultado);
+
 //Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 let mediaAritmetica = resultado / numbers.length
 console.log(mediaAritmetica)
+
 //Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 if (mediaAritmetica > 20) {
-    console.log("Resultado maior que 20")
+    console.log("Valor maior que 20")
 } else {
     console.log("Valor menor ou igual a 20")
 }
+
 //Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 let maiorNumero = 0
-for (let calculaMaior = 0; calculaMaior < numbers.length; calculaMaior += 1) {
-    if (numbers[calculaMaior] > maiorNumero) {
-        maiorNumero = numbers[calculaMaior]
+for (let calculaMaior of numbers) {
+    if (calculaMaior > maiorNumero) {
+        maiorNumero = calculaMaior
     }
 }
 console.log(maiorNumero)
+
 //Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
-let numerosImpares = 0
-for (let index = 0; index < numbers.length; index += 1){
-if (numbers[index] % 2 !== 0) {
-    numerosImpares += 1
+let numerosImpares = []
+for (let odd of numbers){
+if (odd % 2 !== 0) {
+    numerosImpares.push(odd)
     }
 } 
-if (numerosImpares === 0) {
+if (numerosImpares.lenght === 0) {
     console.log("Nenhum valor ímpar encontrado")
 } else {
-    console.log(numerosImpares)
+    console.log(`Foram encontrados números ímpares no array: ${numerosImpares}`)
     }
+
 //Utilizando for , descubra qual o menor valor contido no array e imprima-o;
 let menorNumero = maiorNumero
-for (let calculaMaior = 0; calculaMaior < numbers.length; calculaMaior += 1) {
-    if (numbers[calculaMaior] < menorNumero) {
-        menorNumero = numbers[calculaMaior]
+for (let calculaMaior of numbers) {
+    if (calculaMaior < menorNumero) {
+        menorNumero = calculaMaior
     }
 }
 console.log(menorNumero)
+
 //Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado;
 let x = []
 for (let index = 0; index <= 25; index += 1) {
     x.push(index);
 } console.log(x);
+
 //Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.
-for (let index = 0; index < x.length; index += 1) {
-    console.log((x[index] / 2))
+for (let index of x) {
+    console.log(index / 2)
 }
