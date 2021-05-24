e9f6c18d8451eb65fac07aa91ed3ee5527984e28
Robertson <robertson.nunes@gmail.com>
2021-05-24 19:47:07 -0300

    4.4Exercise3-Refactoring

--- exercises/4.4_Introdução-Javascript/3/scripts.js
+++ exercises/4.4_Introdução-Javascript/3/scripts.js
@@ -1,25 +1,45 @@
 let info = {
-    personagem: ["Margarida", "Tio Patinhas"],
-    origem: ["Pato Donald", "Christmas on Bear Mountain, Dell's Four Color Comics #178"],
-    nota: ["Namorada do personagem principal nos quadrinhos do Pato Donald", "O último MacPatinhas",],
-    recorrente: "Ambos Recorrentes"
+    personagem: ["Margarida"],
+    origem: ["Pato Donald"],
+    nota: ["Namorada do personagem principal nos quadrinhos do Pato Donald"],
   };
 
-// Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
-console.log('Bem vinda ' + info.personagem)
+/*1. Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. */
+console.log(`Bem vinda ${info.personagem}`)
 
-// Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, imprima o objeto no console.
+/* 2. Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, imprima o objeto no console. */
+  info.recorrente = 'sim';
+  console.log(info)
 
-// Faça um for/in que mostre todas as chaves do objeto.
+/* Faça um for/in que mostre todas as chaves do objeto. */
 for (let key in info) {
     console.log(key)
 }
 
-// Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto.
-for (let allKeys in info) {
-    console.log(info[allKeys])
+/* Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. */
+for (let key in info) {
+    console.log(info[key])
 }
 
+/* Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. */
+let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "sim",
+  };
+
+function Grouping() {
+    for (let key in info) {
+        if (info[key] != info2[key]){
+            console.log(`${info[key]} e ${info2[key]}`)   
+        } else {
+            console.log(`Ambos ${key}s`)
+        }      
+    }
+}
+
+Grouping()
 // PARTE II
 // Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
 let arara = "palindromo"
