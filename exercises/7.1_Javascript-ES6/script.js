254b17c97ebbc281ab5420ae8092bfea4b895612
Robertson <robertson.nunes@gmail.com>
2021-06-23 18:37:55 -0300

    JavascriptES6-ExPt1

---
+++ exercises/7.1_Javascript-ES6/script.js
@@ -0,0 +1,47 @@
+/* Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+  1. Modifique a estrutura da função para que ela seja uma arrow function .
+  2. Modifique as concatenações para template literals .
+  3. Copie o código abaixo. */
+
+/*
+function testingScope(escopo) {
+  if (escopo === true) {
+    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+}
+
+testingScope(true); */
+
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    console.log(`${ifScope} ótimo, fui utilizada no escopo !`);
+  } else {
+    let elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+    console.log(`${elseScope}`)
+  }
+}
+
+testingScope(true)
+
+/* Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
+Copie o código abaixo. */
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+
+const sortArray = array => {
+ const sortOddsAndEvens = array.sort((a, b) => a - b);
+  return sortOddsAndEvens;
+}
+
+const sortedArray = sortArray(oddsAndEvens);
+console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente !`);
