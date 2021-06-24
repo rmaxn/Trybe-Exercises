a9460653c53c78a1cad45724763d0b8d71451c22
Robertson <robertson.nunes@gmail.com>
2021-06-24 13:43:24 -0300

    JavascriptES6-ExPt2

--- exercises/7.1_Javascript-ES6/script.js
+++ exercises/7.1_Javascript-ES6/script.js
@@ -45,3 +45,80 @@ const sortArray = array => {
 
 const sortedArray = sortArray(oddsAndEvens);
 console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente !`);
+
+/*
+Abaixo, você verá algumas especificações de algoritmos para desenvolver. É fundamental que você utilize o que aprendeu sobre let , const , arrow functions , template literals e ternary operator .
+  1. Crie uma função que receba um número e retorne seu fatorial.
+    Na matemática, o fatorial de um número não negativo N , com a notação N! , é o produto de todos os inteiros menores ou iguais a N . Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+    Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha usando ternary operator .
+*/
+
+const factorial = number => number > 1 ? number * factorial(number - 1) : 1
+console.log(factorial(5))
+
+/* Crie uma função que receba uma frase e retorne qual a maior palavra. */
+
+const longestWord = string => {
+  let array = string.split(' ')
+  let bigestWordLength = 0
+  let biggestWord = '';
+  for(let index of array) {
+    if (index.length > bigestWordLength) {
+      bigestWordLength = index.length
+      biggestWord = index
+    }
+  }
+  return console.log(biggestWord)
+}
+
+longestWord("Antonio foi no banheiro e não sabemos o que aconteceu")
+
+/* Crie uma página que contenha: 
+Um botão ao qual será associado um event listener ;
+Uma variável clickCount no arquivo JavaScript que acumule o número de clicks no botão;
+Um campo no HTML que vá atualizando a quantidade de clicks no botão conforme a variável clickCount é atualizada.*/
+
+const button = document.querySelector('.clickCount');
+let p = document.createElement('p')
+document.body.appendChild(p);
+let clickCount = 1
+button.addEventListener('click', function() {
+  (clickCount === 1) ? p.innerText = `Clicou 1 vez` : p.innerText = `Clicou ${clickCount} vezes`;
+  clickCount += 1
+})
+
+/* 4. Crie um código JavaScript com a seguinte especificação:
+  Não se esqueça de usar template literals
+
+    Função 1 : Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string .
+      Exemplo:
+        String determinada: "Tryber x aqui!"
+        Parâmetro: "Bebeto"
+        Retorno: "Tryber Bebeto aqui!"
+    Um array com escopo global, que é o escopo do arquivo JS , nesse caso, contendo cinco strings com suas principais skills .
+*/
+
+const searchXLetter = (string, word) => {
+  let letter = 'x';
+  let newString = string.split(' ')
+  let newPhrase = []
+  for(let key in newString) {
+    if(newString[key] === letter) {
+      newPhrase = newString.splice(key, 1, word)
+    }
+  }
+  return newString.join(' ')
+}
+ 
+console.log(searchXLetter('Tryber x aqui', 'Bebeto'))
+
+const array = ['Javascript', 'HTML', 'CSS', 'React', 'Redux'];
+
+/* Função 2 : Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string. */
+
+const concatPhrWSkills = (frase) => {
+  let arrayOrdenado = array.sort().join(', ');
+  let concat = `${frase} Tudo bem? Minhas cinco principais habilidades são: ${arrayOrdenado}! #GoTrybe`
+  return concat
+}
+console.log(concatPhrWSkills(searchXLetter('Tryber x aqui!', 'Bebeto')))
