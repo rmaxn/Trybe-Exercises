e9001bc09b1f192878cd1d3ce4e0069a3283ed22
Robertson <robertson.nunes@gmail.com>
2021-06-28 18:25:04 -0300

    Javascript-ES6-HOFs-2

--- exercises/8.1_Higher-Order-Functions-do-JavaScript/2/script.js
+++ exercises/8.1_Higher-Order-Functions-do-JavaScript/2/script.js
@@ -32,3 +32,24 @@ const check = () => {
   } 
 } 
 check()
+
+/*  3. Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito), o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e quando não houver resposta ("N.A") não altera-se a contagem.
+*/
+
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const gabarito = (rightAnswers, studentAnswers, checkAnswers = () => {
+  let count = 0;
+  for (let index = 0; index < studentAnswers.length; index += 1) {
+    if (rightAnswers[index] === studentAnswers[index]) {
+      count += 1;
+    } else if (studentAnswers[index] !== rightAnswers[index] && studentAnswers[index] !== 'N.A') {
+      count -= 0.5;
+    }
+  }
+  return count;
+})
+
+console.log(gabarito())
