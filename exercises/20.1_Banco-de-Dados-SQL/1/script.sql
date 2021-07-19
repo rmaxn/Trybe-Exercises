b42aad48ac933f982557724558a828b561dbef3a
Robertson <robertson.nunes@gmail.com>
2021-07-19 16:24:09 -0300

    BancoDeDadosSQL

---
+++ exercises/20.1_Banco-de-Dados-SQL/1/script.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `robertson`.`Filme` (
+  `FilmeId` INT NULL AUTO_INCREMENT,
+  `Descricao` VARCHAR(100) NOT NULL,
+  `AnoLancamento` INT NOT NULL,
+  `Nota` INT NULL,
+  PRIMARY KEY (`FilmeId`));
