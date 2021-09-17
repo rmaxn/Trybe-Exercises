5b4032956411cede8393a5ac74917b202030ea01
Robertson <robertson.nunes@gmail.com>
2021-09-17 11:19:34 -0300

    project10

---
+++ projetos/Projeto-10-Zoo-Functions/sd-010-a-project-zoo-functions/src/zoo.js
@@ -0,0 +1,88 @@
+/*
+eslint no-unused-vars: [
+  "error",
+  {
+    "args": "none",
+    "vars": "local",
+    "varsIgnorePattern": "data"
+  }
+]
+*/
+const { animals } = require('./data');
+const data = require('./data');
+
+function animalsByIds(...ids) {
+  return animals.filter((animais) => ids.includes(animais.id));
+}
+
+function animalsOlderThan(animal, age) {
+  let result;
+  animals.map(({ name, residents }) => {
+    if (name === animal) {
+      result = residents.every(({ age: idade }) => idade >= age)
+    }
+    return false
+  })
+  return result
+}
+
+
+function employeeByName(employeeName) {
+  // seu código aqui
+}
+
+function createEmployee(personalInfo, associatedWith) {
+  // seu código aqui
+}
+
+function isManager(id) {
+  // seu código aqui
+}
+
+function addEmployee(id, firstName, lastName, managers, responsibleFor) {
+  // seu código aqui
+}
+
+function animalCount(species) {
+  // seu código aqui
+}
+
+function entryCalculator(entrants) {
+  // seu código aqui
+}
+
+function animalMap(options) {
+  // seu código aqui
+}
+
+function schedule(dayName) {
+  // seu código aqui
+}
+
+function oldestFromFirstSpecies(id) {
+  // seu código aqui
+}
+
+function increasePrices(percentage) {
+  // seu código aqui
+}
+
+function employeeCoverage(idOrName) {
+  // seu código aqui
+}
+
+module.exports = {
+  entryCalculator,
+  schedule,
+  animalCount,
+  animalMap,
+  animalsByIds,
+  employeeByName,
+  employeeCoverage,
+  addEmployee,
+  isManager,
+  animalsOlderThan,
+  oldestFromFirstSpecies,
+  increasePrices,
+  createEmployee,
+};
